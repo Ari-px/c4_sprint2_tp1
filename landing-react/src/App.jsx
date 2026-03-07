52034517db7ca7c4e8eefb7243b2d77640db9955
unknown <arianaespechegmail.com>
2026-03-07 19:35:15 -0300

    Landing Page React Hallyu

---
+++ landing-react/src/App.jsx
@@ -0,0 +1,25 @@
+import Header from "./components/Header"
+import Hero from "./components/Hero"
+import Contact from "./components/Contact"
+import Albums from "./components/Albums"
+import Footer from "./components/Footer"
+
+function App(){
+
+return(
+
+<>
+
+<Header/>
+<Hero/>
+<Albums/>
+<Contact />
+<Footer/>
+
+</>
+
+)
+
+}
+
+export default App
---
+++ landing-react/src/components/Footer.jsx
@@ -0,0 +1,26 @@
+export default function Footer(){
+
+return(
+
+<footer className="bg-pink-300 text-white text-center p-6">
+
+<p>© 2026 Hallyu Store</p>
+
+
+<p className="text-sm mt-2">
+Tienda fan de K-Pop
+</p>
+
+<div className="space-x-4 mt-4 text-xl">
+
+<i className="bi bi-instagram"></i>
+<i className="bi bi-youtube"></i>
+<i className="bi bi-tiktok"></i>
+
+</div>
+
+</footer>
+
+)
+
+}
